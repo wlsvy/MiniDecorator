d3ab0d9f819693c43e281dd3aba4c3470993f9a8
wlsvy <hubjpkim@gmail.com>
2024-10-09 21:52:40 +0900

    테스트 프로젝트는 Generator dll 을 그대로 참조하도록

--- src/MiniDecorator/MiniDecorator.Tests/ParserTest.cs
+++ src/MiniDecorator/MiniDecorator.Tests/ParserTest.cs
@@ -29,57 +29,14 @@ public sealed class TestDecoratorAttribute : DecorateBaseAttribute(template: $$"
   }
   """""";
 ";
-      
       SyntaxTree tree = CSharpSyntaxTree.ParseText(code)!;
       SyntaxNode root = tree.GetRoot()!;
       ClassDeclarationSyntax classDeclarationSyntax = root.DescendantNodes()
           .OfType<ClassDeclarationSyntax>()
           .Single();
       
-      Assert.True(TryGetDecoratorAttribute(classDeclarationSyntax, out PrimaryConstructorBaseTypeSyntax? decoratorConstructor));
-      string parsedTemplate = ParseTemplate(decoratorConstructor!);
+      Assert.True(MiniDecorator.DecoratorSourceGenerator.TryGetDecoratorAttribute(classDeclarationSyntax, out PrimaryConstructorBaseTypeSyntax? decoratorConstructor));
+      string parsedTemplate = MiniDecorator.DecoratorSourceGenerator.ParseTemplate(decoratorConstructor!);
 
    }
-   
-    public static bool TryGetDecoratorAttribute(ClassDeclarationSyntax classDeclarationSyntax, out PrimaryConstructorBaseTypeSyntax? decoratorConstructor)
-    {
-        if (classDeclarationSyntax.BaseList is null)
-        {
-            decoratorConstructor = null;
-            return false;
-        }
-        
-        SeparatedSyntaxList<BaseTypeSyntax> baseListTypes = classDeclarationSyntax.BaseList!.Types;
-        BaseTypeSyntax? decoratorBaseAttributeClass = baseListTypes.SingleOrDefault(t => t.Type.ToString() == nameof(DecorateBaseAttribute));
-        PrimaryConstructorBaseTypeSyntax? primaryConstructorBaseTypeSyntax = decoratorBaseAttributeClass as PrimaryConstructorBaseTypeSyntax;
-        if (primaryConstructorBaseTypeSyntax is null)
-        {
-            decoratorConstructor = null;
-            return false;
-        }
-
-        decoratorConstructor = primaryConstructorBaseTypeSyntax;
-        return true;
-    }
-
-    public static string ParseTemplate(PrimaryConstructorBaseTypeSyntax decoratorDeclaration)
-    {
-        InterpolatedStringExpressionSyntax templateExpression = (InterpolatedStringExpressionSyntax)decoratorDeclaration.ArgumentList.Arguments[0].Expression;
-        StringBuilder parsedTemplate = new ();
-        foreach (InterpolatedStringContentSyntax content in templateExpression.Contents)
-        {
-            if (content is InterpolatedStringTextSyntax text)
-            {
-                // InterpolatedStringText는 그대로 사용
-                parsedTemplate.Append(text.TextToken.ValueText);
-            }
-            else if (content is InterpolationSyntax interpolation)
-            {
-                // Interpolation 구간은 치환
-                parsedTemplate.Append($"##{interpolation.Expression}##");
-            }
-        }
-
-        return parsedTemplate.ToString();
-    } 
 }
