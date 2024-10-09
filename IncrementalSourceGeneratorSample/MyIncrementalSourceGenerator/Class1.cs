9fafbb789a1760c446f92e5ef140514d89d28bb1
wlsvy <hubjpkim@gmail.com>
2024-10-09 18:15:50 +0900

    Incremental Source Generator Sample. 버전 문제로 SourceGenerator 가 동작하지 않는 이유를 찾아보기 위해 미니멀 사이즈 샘플을 새로 만듬

---
+++ IncrementalSourceGeneratorSample/MyIncrementalSourceGenerator/Class1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+[Generator]
+public class HelloWorldGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        // 소스 코드를 생성하는 로직을 추가합니다.
+        context.RegisterSourceOutput(context.CompilationProvider, (spc, compilation) =>
+        {
+            // 간단한 "Hello, World!" 메서드를 생성합니다.
+            var source = @"
+            namespace GeneratedCode
+            {
+                public static class HelloWorld
+                {
+                    public static string SayHello() => ""Hello, World!"";
+                }
+            }";
+
+            // 소스를 추가합니다.
+            spc.AddSource("HelloWorldGenerator", SourceText.From(source, Encoding.UTF8));
+        });
+    }
+}
---
+++ IncrementalSourceGeneratorSample/SampleApp/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+GeneratedCode.HelloWorld.SayHello();
+Console.WriteLine(GeneratedCode.HelloWorld.SayHello());
