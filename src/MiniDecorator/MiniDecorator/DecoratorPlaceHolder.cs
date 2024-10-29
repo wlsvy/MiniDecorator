d8e4fd69b837102fc2b1dac658378ad5905a30b4
김진표 [jinpyo.kim] <jinpyo.kim@devcat.com>
2024-10-29 13:57:41 +0900

    remove unused code

--- src/MiniDecorator/MiniDecorator/DecoratorPlaceHolder.cs
+++ src/MiniDecorator/MiniDecorator/DecoratorPlaceHolder.cs
@@ -7,64 +7,15 @@ namespace MiniDecorator;
 
 public static class DecoratorTemplate
 {
-    public const string MethodName = "##MethodName##";
-    public const string ClassName = "##ClassName##";
-    public const string ReturnType = "##ReturnType##";
-    public const string ParameterList = "##ParameterList##";
-    public const string ParameterListWithType = "##ParameterListWithType##";
-    public const string ParameterList_Skip1 = "##ParameterList_Skip1##";
-    public const string Argument_1 = "##Args_11##";
-    public const string Argument_2 = "##Args_2##";
+    public const string MethodName = $"##{nameof(MethodName)}##";
+    public const string ClassName = $"##{nameof(ClassName)}##";
+    public const string ReturnType = $"##{nameof(ReturnType)}##";
+    public const string ParameterList = $"##{nameof(ParameterList)}##";
+    public const string ParameterListWithType = $"##{nameof(ParameterListWithType)}##";
+    public const string ParameterList_Skip1 = $"##{nameof(ParameterList_Skip1)}##";
+    public const string Argument_1 = $"##{nameof(Argument_1)}##";
+    public const string Argument_2 = $"##{nameof(Argument_2)}##";
 }
 
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-public sealed class DecorateWithAttribute(string template) : Attribute;
-
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Method)]
 public abstract class DecorateBaseAttribute(string template) : Attribute;
-
-public sealed class DecoWithTryCatch() : DecorateBaseAttribute(
-    template:$$"""
-        public {{DecoratorTemplate.ReturnType}} {{DecoratorTemplate.MethodName}}WithTryCatch({{DecoratorTemplate.ParameterListWithType}})
-        {
-            try
-            {
-                return {{DecoratorTemplate.MethodName}}({{DecoratorTemplate.ParameterList}});
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return default;
-            }
-        }
-        """);
-
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Method)]
-public sealed class AutoNotifyAttribute : Attribute;
-
-public partial class Sample
-{
-    public const string Template =$$"""
-         public string Decoreated{{DecoratorTemplate.MethodName}}({{DecoratorTemplate.ParameterList}}}
-         {
-         #if DEBUG
-             System.Console.WriteLine($"Ok, DebugMode Decorator Call...");
-         #endif
-             try
-             {
-                 System.Console.WriteLine($"Before Call");
-                 {{DecoratorTemplate.MethodName}}({{DecoratorTemplate.ParameterList}});
-                 return "Ok, Decoration Completed"
-             }
-             catch (Exception e)
-             {
-                  return $"Exception Occured: {e}";
-             }
-         }
-         """;
-    
-    [DecorateWith(Sample.Template)]
-    private void DoSomething()
-    {
-        //Console.WriteLine($"Do Something");
-    }
-}
