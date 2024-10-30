19419f2963c342e7ceffb9d18fa67875ab99cfaf
김진표 [jinpyo.kim] <jinpyo.kim@devcat.com>
2024-10-30 13:51:45 +0900

    테스트 코드

--- src/MiniDecorator/MiniDecorator.Tests/GeneratorTest.cs
+++ src/MiniDecorator/MiniDecorator.Tests/GeneratorTest.cs
@@ -1,7 +1,9 @@
+using System;
+using Xunit;
+
 namespace MiniDecorator.Tests;
 
-public sealed class DecorateWithTryCatchAttribute() :
-    DecorateBaseAttribute(template: $$"""
+public sealed class DecorateWithTryCatchAttribute() : DecorateBaseAttribute(template: $$"""
                   public {{DecoratorTemplate.ReturnType}} {{DecoratorTemplate.MethodName}}WithTryCatch({{DecoratorTemplate.ParameterListWithType}})
                   {
                       try
@@ -9,7 +11,7 @@ public sealed class DecorateWithTryCatchAttribute() :
                           System.Console.WriteLine($"Before Invoke");
                           return this.{{DecoratorTemplate.MethodName}}({{DecoratorTemplate.ParameterList}});
                       }
-                      catch (Exception e)
+                      catch (System.Exception e)
                       {
                           System.Console.WriteLine($"Error: {e}");
                            return default; 
@@ -17,11 +19,38 @@ public sealed class DecorateWithTryCatchAttribute() :
                   }                    
                   """);
 
-public partial class GeneratorTest
+public sealed class MultiplyTwoAttribute() : DecorateBaseAttribute(template: $$"""
+                  public {{DecoratorTemplate.ReturnType}} {{DecoratorTemplate.MethodName}}AndMultiplyByTwo({{DecoratorTemplate.ParameterListWithType}})
+                  {
+                      return this.{{DecoratorTemplate.MethodName}}({{DecoratorTemplate.ParameterList}}) * 2;
+                  }                    
+                  """);
+
+public partial class SimpleGeneratorTest
 {
+    [Fact]
+    public void DecoratorCompileTest()
+    {
+        this.DoSomethingWithTryCatch();
+    }
+
+    [Fact]
+    public void DecoratorCompileTest2()
+    {
+        long baseResult = this.DoSomething(1, 999, 'a');
+        long decoratedMethodResult = this.DoSomethingAndMultiplyByTwo(1, 999, 'a');
+        Assert.Equal(baseResult * 2, decoratedMethodResult);
+    }
+
     [DecorateWithTryCatch]
     public int DoSomething()
     {
         return 1 + 1;
+    }
+
+    [MultiplyTwo]
+    public long DoSomething(int value1, long value2, char value3)
+    {
+        return value1 + value2 + (int)value3;
     }
 }
